4bba3f0e7981594bd9166f1f83df2487c2c002fc
Aslan <isip_a.g.asalbekov@mpt.ru>
2024-02-26 10:57:44 +0300

    В контейнере

--- logic/subCreator.py
+++ logic/subCreator.py
@@ -5,44 +5,37 @@ import assemblyai as aai
 subsReady = False
 
 
-def create_subtitles():
+def create_subtitles(audio_url):
     global subsReady
     aai.settings.api_key = "5f4d8d4e034a4870b1b180ff4f804f52"
 
-    current_directory = os.path.dirname(os.path.abspath(__file__))
+    try:
+        transcriber = aai.Transcriber()
+        transcript = transcriber.transcribe(audio_url)
+        subs = transcript.export_subtitles_vtt(chars_per_caption=200)
 
-    video_directory = os.path.join(current_directory, '..', 'videos')
-
-    video_file_path = None
-    for filename in os.listdir(video_directory):
-        if filename.endswith('.mp3'):
-            video_file_path = os.path.join(video_directory, filename)
-            break
-
-    if video_file_path:
-        transcriber = aai.Transcriber().transcribe(video_file_path)
-        subs = transcriber.export_subtitles_vtt(chars_per_caption=150)
-
-        subs_directory = os.path.join(current_directory, '..', 'subs')
+        subs_directory = os.path.join('.', 'subs')
         if not os.path.exists(subs_directory):
             os.makedirs(subs_directory)
 
         subs_file_path = os.path.join(subs_directory, 'subs.txt')
         with open(subs_file_path, "w") as f:
             f.write(subs)
+        print(subs)
         subsReady = True
         print("Субтитры готовы")
-    else:
-        print("Нет файлов формата MP3 в папке videos")
+    except Exception as e:
+        print("Ошибка при создании субтитров:", e)
 
 
 def delete_old_subs():
     current_directory = os.path.dirname(os.path.abspath(__file__))
     subs_directory = os.path.join(current_directory, '..', 'subs')
+
     for file_name in os.listdir(subs_directory):
         file_path = os.path.join(subs_directory, file_name)
         try:
             if os.path.isfile(file_path):
                 os.unlink(file_path)
         except Exception as e:
-            print("Error deleting file:", e)
+            print("Ошибка при удалении файла:", e)
