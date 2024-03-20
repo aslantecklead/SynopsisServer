593c15b3244777a99f0fb201abbae67b5eb292d7
Aslan <isip_a.g.asalbekov@mpt.ru>
2024-03-20 17:31:32 +0300

    imported aassemblyai

--- logic/subCreator.py
+++ logic/subCreator.py
@@ -9,7 +9,7 @@ def create_subtitles(video_code):
     global subsReady
     print("Starting subtitle creation process...")
 
-    aai.settings.api_key = "5f4d8d4e034a4870b1b180ff4f804f52"
+    aai.settings.api_key = "0bfaaf47a21c4ab0a1588fe25c7813e7"
 
     current_directory = os.path.dirname(os.path.abspath(__file__))
     subs_directory = os.path.join(current_directory, '..', 'subs')
@@ -31,15 +31,22 @@ def create_subtitles(video_code):
     if video_file_path:
         print("Transcribing audio to subtitles...")
 
-        transcriber = aai.Transcriber().transcribe(video_file_path)
-        subs = transcriber.export_subtitles_vtt(chars_per_caption=150)
+        try:
+            transcriber = aai.Transcriber().transcribe(video_file_path)
+            subs = transcriber.export_subtitles_vtt(chars_per_caption=150)
+            print("1")
 
-        if not os.path.exists(subs_directory):
-            os.makedirs(subs_directory)
+            if not os.path.exists(subs_directory):
+                os.makedirs(subs_directory)
+            print("2")
 
-        with open(subs_file_path, "w") as f:
-            f.write(subs)
-        subsReady = True
-        print("Subtitles are ready")
+            with open(subs_file_path, "w") as f:
+                f.write(subs)
+                print("3")
+
+            subsReady = True
+            print("Subtitles are ready")
+        except Exception as e:
+            print("Error during transcription:", e)
     else:
         print("No MP3 files found in the 'videos' folder")
